c2abd8766f8b90e86a033da83b2b803460dbef51
wonyong <wonyong3927@gmail.com>
2025-03-11 01:23:26 +0000

    look into iostream and argument

---
+++ 2/iostream.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+	int x{};
+	std::cout << "Enter a number: ";
+	std::cin >> x;
+	std::cout << "You entered " << x << std::endl;
+	return 0;
+}
+
